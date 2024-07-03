57a9324b98b00f487cbe5203e64379f9af695e5f
Jillurcf <jrtushar@gmail.com>
2024-07-03 11:59:18 -0700

    SearchScreen created

--- android/app/src/screen/homeSearchScreen/HomeSearchScreen.jsx
+++ android/app/src/screen/homeSearchScreen/HomeSearchScreen.jsx
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import SearchBar from 'react-native-dynamic-search-bar'
+import { allCategories } from '../music'
+const {width, height} = Dimensions.get('window')
 const HomeSearchScreen = () => {
   return (
     <View>
       <Text>HomeSearchScreen</Text>
+      {allCategories.map((category, index) => {
+        return (
+          <View key={index}>
+            <Text>{category.title}</Text>
+            <Text>{category.length}</Text>
+          </View>
+        )
+      })}
     </View>
   )
 }
--- android/app/src/screen/music/index.js
+++ android/app/src/screen/music/index.js
@@ -418,3 +418,417 @@ export const interViewData = [
         },
     
 ]
+
+export const allCategories = [
+     
+    {
+        id: 1,
+        icon: require('../../assets/images/musicOne.jpg'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+        music: 'music1'
+   
+    },
+    {
+        id: 2,
+        icon: require('../../assets/images/musicOne.jpg'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music2'
+   
+    },
+    {
+        id: 3,
+        icon: require('../../assets/images/musicOne.jpg'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music3'
+   
+    },
+    {
+        id: 4,
+        icon: require('../../assets/images/musicOne.jpg'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music1'
+    },
+    {
+        id: 5,
+        icon: require('../../assets/images/musicOne.jpg'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music2'
+   
+    },
+    {
+        id: 6,
+        icon: require('../../assets/images/musicOne.jpg'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music1'
+   
+    },
+    {
+        id: 7,
+        icon: require('../../assets/images/musicOne.jpg'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music2'
+   
+    },
+    {
+        id: 8,
+        icon: require('../../assets/images/musicOne.jpg'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music3'
+   
+    },
+
+
+    {
+        id: 9,
+        videoUrl: "https://videos.pexels.com/video-files/4620563/4620563-uhd_1440_2732_25fps.mp4",
+      icon: require('../../assets/images/videoCover1.png'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+        music: 'music1'
+   
+    },
+    {
+        id: 10,
+        videoUrl: "https://videos.pexels.com/video-files/4620563/4620563-uhd_1440_2732_25fps.mp4",
+      icon: require('../../assets/images/videoCover1.png'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music2'
+   
+    },
+    {
+        id: 11,
+        videoUrl: "https://videos.pexels.com/video-files/4620563/4620563-uhd_1440_2732_25fps.mp4",
+        icon: require('../../assets/images/videoCover1.png'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music3'
+   
+    },
+    {
+        id: 12,
+        videoUrl: "https://videos.pexels.com/video-files/4620563/4620563-uhd_1440_2732_25fps.mp4",
+        icon: require('../../assets/images/videoCover1.png'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music1'
+    },
+    {
+        id: 13,
+        videoUrl: "https://videos.pexels.com/video-files/4620563/4620563-uhd_1440_2732_25fps.mp4",
+        icon: require('../../assets/images/videoCover1.png'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music2'
+   
+    },
+    {
+        id: 14,
+        videoUrl: "https://videos.pexels.com/video-files/4620563/4620563-uhd_1440_2732_25fps.mp4",
+       icon: require('../../assets/images/videoCover1.png'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music1'
+   
+    },
+    {
+        id: 15,
+        videoUrl: "https://videos.pexels.com/video-files/4620563/4620563-uhd_1440_2732_25fps.mp4",
+        icon: require('../../assets/images/videoCover1.png'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music2'
+   
+    },
+    {
+        id: 16,
+        videoUrl: "https://videos.pexels.com/video-files/4620563/4620563-uhd_1440_2732_25fps.mp4",
+        icon: require('../../assets/images/videoCover1.png'),
+        category: "Music",
+        title: "Blake Tinsley – music has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          music: 'music3'
+   
+    },
+
+    {
+        id: 17,
+       
+        icon: require('../../assets/images/NewsCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+        News: 'News1'
+   
+    },
+    {
+        id: 18,
+      
+      icon: require('../../assets/images/NewsCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News2'
+   
+    },
+    {
+        id: 19,
+  
+        icon: require('../../assets/images/NewsCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News3'
+   
+    },
+    {
+        id: 20,
+   
+        icon: require('../../assets/images/NewsCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News1'
+    },
+    {
+        id: 21,
+    
+        icon: require('../../assets/images/NewsCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News2'
+   
+    },
+    {
+        id: 22,
+      
+       icon: require('../../assets/images/NewsCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News1'
+   
+    },
+    {
+        id: 23,
+     
+        icon: require('../../assets/images/NewsCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News2'
+   
+    },
+    {
+        id: 24,
+    
+        icon: require('../../assets/images/NewsCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News3'
+   
+    },
+
+    {
+        id: 25,
+     
+        icon: require('../../assets/images/InterviewCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+        News: 'News1'
+   
+    },
+    {
+        id: 26,
+      
+      icon: require('../../assets/images/InterviewCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News2'
+   
+    },
+    {
+        id: 27,
+  
+        icon: require('../../assets/images/InterviewCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News3'
+   
+    },
+    {
+        id: 28,
+    
+        icon: require('../../assets/images/InterviewCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News1'
+    },
+    {
+        id: 29,
+     
+        icon: require('../../assets/images/InterviewCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News2'
+   
+    },
+    {
+        id: 30,
+     
+       icon: require('../../assets/images/InterviewCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News1'
+   
+    },
+    {
+        id: 31,
+     
+        icon: require('../../assets/images/InterviewCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News2'
+   
+    },
+    {
+        id: 32,
+   
+        icon: require('../../assets/images/InterviewCoverImg.png'),
+        category: "News",
+        title: "Blake Tinsley – News has always been my refuge",
+        singer: "Tony M Fountain",
+        date: "Nov 18,2023",
+        address: "Blake Tinslay,Fort Hood Texas,oh well",
+        description: "Blake Tinsley was born at Fort Hood, Texas, and grew up the son of a single mother who served in the U.S. Army...",
+          News: 'News3'
+   
+    },
+
+
+]
